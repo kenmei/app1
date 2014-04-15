58273006e174473a97e2966ed7fb63bd92fc34fe
kenmei <kenmei.nn@gmail.com>
2014-04-15 20:37:15 -0300

    Initial Commit

---
+++ Loja.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Loja.LojaVirtual.UnitTest {
+    [TestClass]
+    public class UnitTestLoja {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
---
+++ Loja.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Loja.LojaVirtual.Web.MvcApplication" Language="C#" %>
